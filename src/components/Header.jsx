91eb6143a11826972cd7be555d468f8eaa4278e1
Your Name <you@example.com>
2024-06-05 16:22:58 +0800

    Seperating Header and  Footer

---
+++ src/components/Header.jsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import Aboutme from "./Aboutme.jsx";
+import Resume from "./Resume.jsx";
+import "../component-CSS/Header.css";
+
+function App() {
+  const [currentSelection, setCurrentSelection] = useState(null);
+
+  const showAbout = () => {
+    setCurrentSelection("about");
+  };
+
+  const showResume = () => {
+    setCurrentSelection("resume");
+  };
+
+  return (
+    <div >
+      <div id="header-Container">
+        <section>
+          <h1>J</h1>
+        </section>
+
+        <div>
+          <h2 id="applicant-Name">
+            <strong>Joshua</strong> Del Rosario
+          </h2>
+        </div>
+
+        <section id="button-Container">
+          <ul>
+            <li>
+              <button className="header-Button" onClick={() => showAbout()}>
+                About Me
+              </button>
+            </li>
+            <li>
+              <button className="header-Button" onClick={() => showResume()}>
+                Resume
+              </button>
+            </li>
+            <li>
+              <button className="header-Button">Portfolio</button>
+            </li>
+            <li>
+              <button className="header-Button">blog</button>
+            </li>
+            <li>
+              <button className="header-Button">Contact</button>
+            </li>
+            <li>
+              <button id="header-Hireme">Hire Me</button>
+            </li>
+          </ul>
+        </section>
+      </div>
+      {currentSelection === "about" && <Aboutme />}
+      {currentSelection === "resume" && <Resume />}
+    </div>
+  );
+}
+
+export default App;
